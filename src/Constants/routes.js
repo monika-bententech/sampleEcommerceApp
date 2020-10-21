1502dd987acf82e3c299f0d653a1103605ff2467
Subha Mishra <subhadarshi@softsuave.com>
2020-10-22 00:38:02 +0530

    ---> Initial commit for ProductDetails screen

--- src/Constants/routes.js
+++ src/Constants/routes.js
@@ -1,10 +1,14 @@
 import { SCREEN_NAMES } from ".";
 import Home from "../Screens/Home";
-
+import ProductDetails from "../Screens/ProductDetails";
 
 export const SCREEN_ROUTES = [
-    {name: SCREEN_NAMES.home, key: 'home_screen', component: Home },
-    {name: SCREEN_NAMES.cart, key: 'shopping_cart', component: null },
-    {name: SCREEN_NAMES.wishlist, key: 'wish_list', component: null },
-    {name: SCREEN_NAMES.productDetail, key: 'product_details', component: null },
-]
+  { name: SCREEN_NAMES.home, key: "home_screen", component: Home },
+  { name: SCREEN_NAMES.cart, key: "shopping_cart", component: null },
+  { name: SCREEN_NAMES.wishlist, key: "wish_list", component: null },
+  {
+    name: SCREEN_NAMES.productDetail,
+    key: "product_details",
+    component: ProductDetails,
+  },
+];
