807107317adddbb1f8e49278f149db755d61afc4
Subha Mishra <subhadarshi@softsuave.com>
2020-10-22 00:54:49 +0530

    ---> Added assets and changes related to ProductDetails screen

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,14 +1,30 @@
+import Pic1 from "../Assets/Images/p1.jpg";
+import Pic2 from "../Assets/Images/p2.jpg";
+import Pic3 from "../Assets/Images/p3.jpg";
 
 export const SCREEN_NAMES = {
-    home: 'Home',
-    cart: 'Shopping Cart',
-    wishlist: 'Wishlist',
-    productDetail: 'Product Detail'
-}
+  home: "Home",
+  cart: "Shopping Cart",
+  wishlist: "Wishlist",
+  productDetail: "Product Detail",
+};
 
 export const COLORS = {
-    white: '#fff',
-    black: '#000',
-    statusBar: '#66CCCE',
-    headerBackground: '#81D8E1'
-}
+  white: "#fff",
+  black: "#000",
+  statusBar: "#66CCCE",
+  headerBackground: "#81D8E1",
+};
+
+export const PRODUCT_LIST = [
+  { id: "key_1", name: "Eveready", imgSrc: Pic1, price: 20, qty: 10,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.' },
+  { id: "key_2", name: "Eveready", imgSrc: Pic2, price: 30, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_3", name: "Eveready", imgSrc: Pic3, price: 20, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_4", name: "Eveready", imgSrc: Pic1, price: 10, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_5", name: "Eveready", imgSrc: Pic2, price: 30, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_6", name: "Eveready", imgSrc: Pic3, price: 40, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_7", name: "Eveready", imgSrc: Pic1, price: 40, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_8", name: "Eveready", imgSrc: Pic2, price: 60, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_9", name: "Eveready", imgSrc: Pic3, price: 70, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+  { id: "key_10", name: "Eveready", imgSrc: Pic1, price: 120, qty: 10 ,desc:'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'},
+];
